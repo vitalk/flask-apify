93aecae6856ed84abd01bdc3dbfa89e7e13946c0
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-08-10 15:22:54 +0300

    Update version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name='flask-apify',
-    version='0.2',
+    version='0.3.0',
     license='MIT',
     author='Vital Kudzelka',
     author_email='vital.kudzelka@gmail.com',
