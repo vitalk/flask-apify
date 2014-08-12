706c22392ca24aea7704a984ad891665167850ea
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-08-12 21:55:44 +0300

    Add test suite for JSON-P serializer

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -4,7 +4,9 @@ import pytest
 
 from flask.ext.apify.serializers import Serializer
 from flask.ext.apify.serializers import JSONSerializer
+from flask.ext.apify.serializers import JSONPSerializer
 from flask.ext.apify.serializers import DebugSerializer
+from flask.ext.apify.serializers.jsonp import jsonp
 
 
 class TestSerializer(object):
@@ -34,3 +36,33 @@ class TestJSONSerializer(object):
 
     def test_dump(self):
         assert self.serializer({'ping': 'pong'}) == '{"ping": "pong"}'
+
+
+class TestJSONPSerializer(object):
+
+    def setup(self):
+        self.serializer = JSONPSerializer()
+
+    def test_add_padding_to_string(self):
+        assert jsonp('{"ping": "pong"}', 'console.log') == \
+                'console.log({"ping": "pong"});'
+
+    def test_returns_string_as_is_if_no_padding(self):
+        assert jsonp('hello') == 'hello'
+
+    def test_use_previously_registered_serializer_to_dump_json(self, webapp, apify):
+        @apify.serializer('application/json')
+        def my_json(raw):
+            return '42'
+
+        with webapp.test_request_context():
+            assert self.serializer('What is the meaning of the Life?') == '42'
+
+    def test_use_callback_function_from_request_arguments_to_wrap_output(self, webapp):
+        with webapp.test_request_context('?callback=console.log'):
+            assert self.serializer('42') == 'console.log(42);'
+
+    def test_support_custom_callback_name(self, webapp):
+        serializer = JSONPSerializer(callback_name='jsonp')
+        with webapp.test_request_context('?jsonp=console.log'):
+            assert serializer('42') == 'console.log(42);'
