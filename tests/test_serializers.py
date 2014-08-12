277152ef62f59c9c96b9ee5c2183ce28bceb392f
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-08-12 11:06:46 +0300

    Add test suite for JSON serializer

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -3,6 +3,7 @@
 import pytest
 
 from flask.ext.apify.serializers import Serializer
+from flask.ext.apify.serializers import JSONSerializer
 
 
 class TestSerializer(object):
@@ -13,3 +14,12 @@ class TestSerializer(object):
     def test_base_class_raises_not_implementent_error_on_call(self):
         with pytest.raises(NotImplementedError):
             self.serializer({})
+
+
+class TestJSONSerializer(object):
+
+    def setup(self):
+        self.serializer = JSONSerializer()
+
+    def test_dump(self):
+        assert self.serializer({'ping': 'pong'}) == '{"ping": "pong"}'
